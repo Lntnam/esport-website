7be5674879d7b11b2b967ecd45fdd533c356406a
Nam Le <j251282@gmail.com>
2016-09-14 18:43:45 +0700

    some code styling

--- app/Http/Controllers/Back/MatchController.php
+++ app/Http/Controllers/Back/MatchController.php
@@ -12,6 +12,7 @@ use App\Repositories\MatchRepository;
 use App\Repositories\OpponentRepository;
 use App\Repositories\TournamentRepository;
 use Illuminate\Http\Request;
+use Setting;
 use Validator;
 
 class MatchController extends BaseController
