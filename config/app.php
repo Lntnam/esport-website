030075d4d060201e99659beffdf7dc209a9be7a4
Nam Le <j251282@gmail.com>
2016-09-16 05:49:55 +0700

    merge app service provider

--- config/app.php
+++ config/app.php
@@ -183,9 +183,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\CountryListServiceProvider::class,
-        App\Providers\SettingServiceProvider::class,
-        App\Providers\MailChimpServiceProvider::class
     ],
 
     /*
