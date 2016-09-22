58172ab877d7689009839087d8c79f2050ed3560
Nam Le <j251282@gmail.com>
2016-09-23 03:03:01 +0700

    ckeditor

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -1,31 +1,37 @@
 /**
  * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
- *
- * To be used in inline editors only.
  */
 
 CKEDITOR.editorConfig = function (config) {
-    config.skin = 'bootstrapck';
     config.forcePasteAsPlainText = true;
+    config.enterMode = CKEDITOR.ENTER_BR;
+    config.entities = false;
+    config.htmlEncodeOutput = false;
+    config.extraPlugins = 'glyphicons';
+    config.contentsCss = '../css/bootstrap.min.css';
+    config.allowedContent = true;
     config.toolbar = [
+        {name: 'document', items: ['Sourcedialog', '-', 'Inlinesave']},
         {name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
-        {name: 'editing', items: ['Scayt']},
-        {name: 'links', items: ['Link', 'Unlink', 'Anchor']},
-        {name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'SpecialChar']},
-        {name: 'document', items: ['Inlinesave']},
+        {name: 'styles', items: ['Format', 'FontSize']},
+        {name: 'colors', items: ['TextColor', 'BGColor']},
         '/',
         {
             name: 'basicstyles',
-            items: ['Bold', 'Italic', 'Strike', 'Underline', '-', 'Subscript', 'Superscript', '-', 'RemoveFormat']
+            items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']
         },
-        {name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote']},
-        {name: 'styles', items: ['Format']}
+        {
+            name: 'paragraph',
+            items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']
+        },
+        {name: 'links', items: ['Link', 'Unlink']},
+        {name: 'insert', items: ['Glyphicons']}
     ];
     config.inlinesave = {
         postUrl: postUrl,
         postData: {_token: token},
-        onSuccess: function(editor, data) {
+        onSuccess: function (editor, data) {
             console.log(data);
             data = $.parseJSON(data);
             if (data.success != true) {
@@ -34,7 +40,4 @@ CKEDITOR.editorConfig = function (config) {
             editor.focusManager.blur();
         },
     };
-    config.enterMode = CKEDITOR.ENTER_BR;
-    config.entities = false;
-    config.htmlEncodeOutput = false;
 };
