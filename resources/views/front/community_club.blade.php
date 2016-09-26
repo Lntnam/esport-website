f5be4d6d540863135013e69d49e2e30b4ff543d6
Nam Le <j251282@gmail.com>
2016-09-26 20:58:18 +0700

    temporarily disable subscription button paypal

--- resources/views/front/community_club.blade.php
+++ resources/views/front/community_club.blade.php
@@ -144,28 +144,28 @@
                         data-editable="true">{!! ContentBlock::output($view_name, 'heading_payment') !!}</h3>
                     <div class="lead" id="payment_details"
                          data-editable="true">{!! ContentBlock::output($view_name, 'payment_details') !!}</div>
-                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                        <input type="hidden" name="cmd" value="_s-xclick">
-                        <input type="hidden" name="hosted_button_id" value="CTYJ7QU7VLWHU">
-                        <table>
-                            <tr>
-                                <td><input type="hidden" name="on0" value="Phone number">Phone number</td>
-                            </tr>
-                            <tr>
-                                <td><input type="text" name="os0" maxlength="200"></td>
-                            </tr>
-                        </table>
-                        <input type="image"
-                               src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif"
-                               border="0"
-                               name="submit"
-                               alt="PayPal - The safer, easier way to pay online!">
-                        <img alt=""
-                             border="0"
-                             src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
-                             width="1"
-                             height="1">
-                    </form>
+                    {{--<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">--}}
+                        {{--<input type="hidden" name="cmd" value="_s-xclick">--}}
+                        {{--<input type="hidden" name="hosted_button_id" value="CTYJ7QU7VLWHU">--}}
+                        {{--<table>--}}
+                            {{--<tr>--}}
+                                {{--<td><input type="hidden" name="on0" value="Phone number">Phone number</td>--}}
+                            {{--</tr>--}}
+                            {{--<tr>--}}
+                                {{--<td><input type="text" name="os0" maxlength="200"></td>--}}
+                            {{--</tr>--}}
+                        {{--</table>--}}
+                        {{--<input type="image"--}}
+                               {{--src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif"--}}
+                               {{--border="0"--}}
+                               {{--name="submit"--}}
+                               {{--alt="PayPal - The safer, easier way to pay online!">--}}
+                        {{--<img alt=""--}}
+                             {{--border="0"--}}
+                             {{--src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"--}}
+                             {{--width="1"--}}
+                             {{--height="1">--}}
+                    {{--</form>--}}
 
                 </div>
                 <div class="col-lg-10 col-lg-offset-1" style="text-align: center">
